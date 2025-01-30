ca747a4cbe3a4f37bf9d5d87655f4b4bf113fa32
Ashish Pandey <pandeyashish0507@gmail.com>
2025-01-30 22:26:32 +0530

    check string palindrome using while loop

---
+++ palindromestring.py
@@ -0,0 +1,12 @@
+def palindrome(n):
+    l = 0
+    r = len(n) - 1 
+    while (l != r):
+        if n[l] != n[r]:
+            return False
+        l += 1
+        r -= 1
+    return True
+
+a = "cioooouh"
+print(palindrome(a))
